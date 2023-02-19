fa1bfa1a76d688204826875329d57858cb9098f0
Diego Arce <Diego@arce.cr>
2023-02-19 11:35:08 -0600

    fix: data dir for cronjobs

--- on-boot-script-2.x/examples/udm-files/on_boot.d/25-add-cron-jobs.sh
+++ on-boot-script-2.x/examples/udm-files/on_boot.d/25-add-cron-jobs.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
 
-## Store crontab files in /mnt/data/cronjobs/ (you will need to create this folder).
+## This script will add cronjobs from /mnt/data/cronjobs/ or /data/cronjobs/ on boot.
+## The path can be personalized by changing the CRONJOBS_DIR variable.
 ## This script will re-add them on startup.
 
-cp /mnt/data/cronjobs/* /etc/cron.d/
+# Get DataDir location
+DATA_DIR="/mnt/data"
+case "$(ubnt-device-info firmware || true)" in
+    1*)
+      DATA_DIR="/mnt/data"
+      ;;
+    2*)
+      DATA_DIR="/data"
+      ;;
+    3*)
+      DATA_DIR="/data"
+      ;;
+    *)
+      echo "ERROR: No persistent storage found." 1>&2
+      exit 1
+      ;;
+  esac
+
+## Adjust this to your needs
+$CRONJOBS_DIR="$DATA_DIR/cronjobs"
+
+# Add cronjob files
+cp $CRONJOBS_DIR/* /etc/cron.d/
 /etc/init.d/crond restart
 
 exit 0
